44a7d1815e084d68dec23efd2c5af91af57e7a15
Psmithcode <peytonsmith53@gmail.com>
2022-09-04 17:15:10 -0500

    Honestly a lot stuff

--- script.js
+++ script.js
@@ -5,12 +5,44 @@ let aboutButton = document.getElementById("aboutButton");
 let contactButton = document.getElementById("contactButton");
 
 let aboutContainer = document.getElementById("aboutContainer");
+let contactContainer = document.getElementById("contactContainer");
+let resumeContainer = document.getElementById("resumeContainer");
+let projectsContainer = document.getElementById("projectsContainer");
 
 const setActive = (element) => {
   removeActive();
   element.firstChild.classList.toggle("hide");
   element.classList.toggle("active");
   console.log([element]);
+};
+
+const setDisplay = (element) => {
+  if (homeButton.classList.contains("active")) {
+    aboutContainer.style.opacity = "0";
+    contactContainer.style.opacity = "0";
+    resumeContainer.style.opacity = "0";
+    projectsContainer.style.opacity = "0";
+  } else if (aboutButton.classList.contains("active")) {
+    aboutContainer.style.opacity = "1";
+    contactContainer.style.opacity = "0";
+    resumeContainer.style.opacity = "0";
+    projectsContainer.style.opacity = "0";
+  } else if (contactButton.classList.contains("active")) {
+    aboutContainer.style.opacity = "0";
+    contactContainer.style.opacity = "1";
+    resumeContainer.style.opacity = "0";
+    projectsContainer.style.opacity = "0";
+  } else if (resumeButton.classList.contains("active")) {
+    aboutContainer.style.opacity = "0";
+    contactContainer.style.opacity = "0";
+    resumeContainer.style.opacity = "1";
+    projectsContainer.style.opacity = "0";
+  } else if (projectsButton.classList.contains("active")) {
+    aboutContainer.style.opacity = "0";
+    contactContainer.style.opacity = "0";
+    resumeContainer.style.opacity = "0";
+    projectsContainer.style.opacity = "1";
+  }
 };
 
 const removeActive = () => {
@@ -26,12 +58,4 @@ const removeActive = () => {
     }
     console.log("hideEL", hideEl);
   });
-};
-
-const setDisplay = (element) => {
-  // let hideEl = element.firstChild.classList.contains("hide");
-  // let activeEl = element.classList.contains("active");
-  while (aboutButton.classList.contains("active")) {
-    aboutContainer.style.opacity = "1";
-  }
 };
